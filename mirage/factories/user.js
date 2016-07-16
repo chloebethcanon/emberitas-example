19caad125b1c738db12ff73dc11a41f610d0c914
Chloe Canon <chloebethcanon@gmail.com>
2016-07-16 16:02:42 -0500

    Add mirage model and factory

---
+++ mirage/factories/user.js
@@ -0,0 +1,10 @@
+import { Factory, faker } from 'ember-cli-mirage';
+
+export default Factory.extend({
+	name() {
+		return faker.name.firstName();
+	},
+	color(i) {
+		return faker.list.random('red', 'yellow', 'blue', 'green', 'teal', 'purple', 'pink')(i);
+	}
+});
