501baef19d5c427e5ef61dd26c0e3110c07f3148
Chloe Canon <chloebethcanon@gmail.com>
2016-07-16 14:58:25 -0500

    Begin correcting cross-site scripting vulnerabilities

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -3,6 +3,9 @@ import Ember from 'ember';
 export default Ember.Controller.extend({
 	name: 'Chloe',
 	color: 'green',
+	someInformation: Ember.computed('name', 'color', function(){
+		return 'Your name is ' + this.get('name') + ' and your favorite color is ' + this.get('color') + ".";
+	}),
 	actions: {
 		setName(newName) {
 			this.set('name', newName);
