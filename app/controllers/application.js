bb75c787c74927dfdc2b821d90f2c4d2f5e8119b
Chloe Canon <chloebethcanon@gmail.com>
2016-07-16 12:34:06 -0500

    Add change color input

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -2,9 +2,13 @@ import Ember from 'ember';
 
 export default Ember.Controller.extend({
 	name: 'Chloe',
+	color: 'green',
 	actions: {
 		setName(newName) {
 			this.set('name', newName);
+		},
+		setColor(newColor) {
+			this.set('color', newColor);
 		}
 	}
 });
