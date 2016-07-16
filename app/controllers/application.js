2d289190ce96993d58290c439c2e248d6ecc463a
Chloe Canon <chloebethcanon@gmail.com>
2016-07-16 15:25:23 -0500

    Add profile controller and move code out of application controller, template

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -1,22 +1,4 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
-	name: 'Chloe',
-	color: 'green',
-	someInformation: Ember.computed('name', 'color', function(){
-		return 'Your name is ' + this.get('name') + ' and your favorite color is ' + this.get('color') + ".";
-	}),
-	colorStyle: Ember.computed('color', function(){
-		// Not necessarily browser compatible
-		let color = CSS.escape(this.get('color'));
-		return Ember.String.htmlSafe('color: ' + color);
-	}),
-	actions: {
-		setName(newName) {
-			this.set('name', newName);
-		},
-		setColor(newColor) {
-			this.set('color', newColor);
-		}
-	}
 });
